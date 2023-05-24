1b0c23913474abde7d19ec859c32b90299e664e5
Max Martinez <maximartinez@me.com>
2023-05-24 14:23:39 +0100

    created models

---
+++ app/models/restaurant.rb
@@ -0,0 +1,6 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  validates :name, presence: true
+  validates :address, presence: true
+  validates_inclusion_of :category, in: ["chinese", "italian", "japanese", "french", "belgian"]
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'restaurants/index'
+  get 'restaurants/show'
+  get 'restaurants/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :restaurants
 end
